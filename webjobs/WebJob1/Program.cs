570163216ecec36afbbe5be0589d6c48fed32634
Cedric Arnould <cedric@sidekickinteractive.com>
2018-01-19 22:48:25 +0000

    Merged PR 410: Setup (WebApp + WebJob)
    Setup (WebApp + WebJob)

---
+++ webjobs/WebJob1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WebJob1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var jobHostConfiguration = new JobHostConfiguration
+            {
+                DashboardConnectionString = configuration.GetConnectionString("AzureWebJobsDashboard"),
+                StorageConnectionString = configuration.GetConnectionString("AzureWebJobsStorage")
+            };
+
+            var host = new JobHost(jobHostConfiguration);
+            host.Call(typeof(Program).GetMethod("Execute"));
+        }
+
+        [NoAutomaticTrigger]
+        public static void Execute(TextWriter logger)
+        {
+            logger.WriteLine("Hello World");
+        }
+    }
+}

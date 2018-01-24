054b0c4e8da33c9bacf9e1f98fbcb3447b7a8fae
Cedric Arnould <cedric@sidekickinteractive.com>
2018-01-24 19:44:47 +0000

    Merged PR 414: .
    .

---
+++ src/InteractActMobilityDeploy/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InteractActMobilityDeploy.Models;
+using Microsoft.Extensions.Logging;
+
+namespace InteractActMobilityDeploy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILogger<HomeController> _log;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            _log = log;
+        }
+
+        public IActionResult Index()
+        {
+            _log.LogInformation("Hello, world!");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/InteractActMobilityDeploy/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using InterActMobilityCommon.Data.Contexts;
+
+namespace InteractActMobilityDeploy
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var initializer = scope.ServiceProvider.GetService<MasterDbInitializer>();
+                initializer.Seed();
+            }
+
+            return host;
+        }
+    }
+}
